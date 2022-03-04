4e52bf582bc2d481b5ebd0f6bcc9786ed55ae305
BT_1E10_13 <sample@bths.edu>
2022-03-04 11:40:15 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,50 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] arr = {{1, 1, 1, 0, 0 ,0}, {0, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0}, {0, 0, 2, 4, 4, 0}, {0, 0, 0, 2, 0, 0}, {0, 0, 1, 2, 4, 0}};
-        System.out.println(hourGlassSum(arr));
-
+        int[][] arr1 = {{-9, -9, -9, 1, 1, 1},
+                {0, -9, 0, 4, 3, 2},
+                {-9, -9, -9, 1, 2, 3},
+                {0, 0, 8, 6, 6, 0},
+                {0, 0, 0, -2, 0, 0},
+                {0, 0, 1, 2, 4, 0}};
+        System.out.println("--------------Test Case 1--------------");
+        System.out.println("Input: ");
+        for (int[] row : arr1){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("Expected output: 28");
+        System.out.println("Output: " + hourGlassSum(arr1));
+        int[][] arr2 = {{1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}};
+        System.out.println("--------------Test Case 2--------------");
+        System.out.println("Input: ");
+        for (int[] row : arr2){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("Expected output: 19");
+        System.out.println("Output: " + hourGlassSum(arr2));
+        int[][] arr3 = {{0, 1, 2, 3, 4, 5},{1, 2, 3, 4, 5, 6},{2, 3, 4, 5, 6, 7},{3, 4, 5, 6, 7, 8}, {4 , 5, 6, 7, 8, 9}, {5 , 6, 7, 8, 9, 9}};
+        System.out.println("--------------Test Case 3--------------");
+        System.out.println("Input: ");
+        for (int[] row : arr3){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("Expected output: 55");
+        System.out.println("Output: " + hourGlassSum(arr3));
     }
     public static int hourGlassSum(int[][] arr){
         ArrayList<Integer> sums = new ArrayList<Integer>();
         for (int r = 0; r < arr.length - 2; r++){
-            int sum = 0;
             for (int c = 0; c < arr[0].length - 2; c++){
-                ArrayList<Integer> positions = hourGlassPosition(r, c);
-                for (int i = 0; i < positions.size() - 2; i++){
-                    sum += arr[positions.get(i)][positions.get(i + 1)];
-                    sums.add(sum);
-                    i++;
-                }
-                System.out.println(sum);
+                int sum = 0;
+                sum += arr[r][c] + arr[r][c + 1] + arr[r][c + 2] + arr[r + 1][c + 1] + arr[r + 2][c] + arr[r + 2][c + 1] + arr[r + 2][c + 2];
+                sums.add(sum);
             }
         }
         int highest = sums.get(0);
@@ -27,23 +54,5 @@ public class Main {
             }
         }
         return highest;
-    }
-    private static ArrayList<Integer> hourGlassPosition(int row, int col){
-        ArrayList<Integer> positions = new ArrayList<Integer>();
-        positions.add(row);
-        positions.add(col);
-        positions.add(row);
-        positions.add(col + 1);
-        positions.add(row);
-        positions.add(col + 2);
-        positions.add(row + 1);
-        positions.add(col + 1);
-        positions.add(row + 2);
-        positions.add(col);
-        positions.add(row + 2);
-        positions.add(col + 1);
-        positions.add(row + 2);
-        positions.add(col + 2);
-        return positions;
     }
 }
